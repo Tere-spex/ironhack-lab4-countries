e5a3b18461d278103337db367f454c2929c26c43
tereh <terehdz.th@gmail.com>
2022-08-10 11:19:14 +0200

    Instalacion de tailwind y fetch ok

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import './tailwind.css';
 import { routes } from "./routes.js";
 import { createRouter, createWebHistory } from "vue-router";
 
